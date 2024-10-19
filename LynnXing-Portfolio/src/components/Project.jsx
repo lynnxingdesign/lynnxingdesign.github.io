66274a7157a4219d94233c9c5e3135167669d2ec
Mythventor <151417971+Mythventor@users.noreply.github.com>
2024-10-19 10:38:54 -0500

    LynnXing Portfolio

--- LynnXing-Portfolio/src/components/Project.jsx
+++ LynnXing-Portfolio/src/components/Project.jsx
@@ -1,27 +1,63 @@
-import React from 'react'
-import ProjectItem from './ProjectItem'
-import SnakeGameProjectImg from '/src/assets/SnakeGameProject.gif'
+import React from 'react';
+import ProjectItem from './ProjectItem';
 
-{/* Import project image */}
-import SpeechRecognitionProjectImg from '/src/assets/SpeechRecognitionProject.png'
-import MatherChessProjectImg from '/src/assets/MatherChessProject.png'
-import PersonalWebProject from '/src/assets/PersonalWebProject.png'
+// Electrolit image
+import Electrolit1 from '/src/assets/Electrolit1Project.png';
+import Electrolit2 from '/src/assets/Electrolit2Project.png';
+import Electrolit3 from '/src/assets/Electrolit3Project.png';
+import Electrolit4 from '/src/assets/Electrolit4Project.png';
+import Electrolit5 from '/src/assets/Electrolit5Project.png';
+
+// Dell
+import Dell1 from '/src/assets/DellSc1Project.jpg';
+import Dell2 from '/src/assets/DellSc2Project.jpg';
+import Dell3 from '/src/assets/DellSc3Project.jpg';
+import Dell4 from '/src/assets/DellSc4Project.jpg';
+
+
 
 const Project = () => {
+  const projects = [
+    {
+      images: [Electrolit1, Electrolit2, Electrolit3, Electrolit4, Electrolit5],
+      title: 'Electrolit NACS 2024',
+
+    },
+    {
+      images: [Dell1, Dell2, Dell3, Dell4],
+      title: 'Dell SC 2024',
+
+    },
+    {
+      images: [Electrolit3, Electrolit4, Electrolit5],
+      title: 'Portfolio',
+
+    },
+    {
+      images: [Electrolit4, Electrolit5, Electrolit1],
+      title: 'Chess Game',
+
+    },
+    {
+      images: [Electrolit5, Electrolit1, Electrolit2],
+      title: 'Another Project',
+    }
+  ];
+
   return (
-    <div id='project' className='max-w-[1048px] m-auto md:pl-20 p-4 py-16'>
-        {/* Project Title, Programming Language, and Link */}
-        <div className='grid sm:grid-cols-2 gap-12'>
-            <ProjectItem img={SnakeGameProjectImg} title='Snake Game' ProgrammingLanguage='DrRacket' ProjectLink='https://github.com/Mythventor/SnakeGame.git'></ProjectItem>
-            <ProjectItem img={SpeechRecognitionProjectImg} title='Speech Recognition App' ProgrammingLanguage='Python' ProjectLink='https://github.com/Mythventor/Voice-To-Text.git'> </ProjectItem>
-            <ProjectItem img={PersonalWebProject} title='Personal Portfolio' ProgrammingLanguage='React+ViteJS+TailwindCSS' ProjectLink='https://github.com/Mythventor/Mythventor.github.io.git'></ProjectItem>
-            <ProjectItem img={MatherChessProjectImg} title='Mather HS Chess Website' ProgrammingLanguage='React+CSS+JS' ProjectLink='https://mythventor.github.io/mather-chess/'></ProjectItem>
+    <div id='project' className='max-w-[1048px] m-auto md:pl-1 p-1 py-16'>
+      <div className='grid sm:grid-cols-1 gap-16'>
+        {projects.map((project, index) => (
+          <ProjectItem
+            key={index}
+            images={project.images}
+            title={project.title}
 
-        </div>
-
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Project
-
+export default Project;
--- LynnXing-Portfolio/src/components/ProjectItem.jsx
+++ LynnXing-Portfolio/src/components/ProjectItem.jsx
@@ -1,25 +1,17 @@
-import React from 'react'
+import React from 'react';
+import ImageSlider from './ImageSlider';
 
-const ProjectItem = ({img, title, ProgrammingLanguage, ProjectLink}) => {
+const ProjectItem = ({ images, title, ProgrammingLanguage, ProjectLink }) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover: bg-gradient-to-r from-gray-200 to-[#001b5e]'>
-        <img src={img} alt="/" className='rounded-xl group-hover:opacity-10 object-cover w-full h-full' />
-        <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-            <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
-                {title}
-            </h3>
-            <p className='pb-4 pt-2 text-white text-center'>
-                {ProgrammingLanguage}
-            </p>
-
-            <a href={ProjectLink} target='_blank'>
-                <p className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg'>
-                    More info
-                </p>
-            </a>
-        </div>
+    <div className='relative h-[600px] w-full shadow-xl shadow-gray-400 rounded-xl'>
+      <ImageSlider images={images} />
+      <div className='absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 p-4 rounded-b-xl'>
+        <h3 className='text-2xl font-bold text-white tracking-wider'>
+          {title}
+        </h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectItem
+export default ProjectItem;
