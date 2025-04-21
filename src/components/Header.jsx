7a3793eec11fc08e43cb2f8e0d7f8ab318a781da
Xing <151417971+Mythventor@users.noreply.github.com>
2025-04-21 18:28:37 -0500

    Update portfolio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from '../assets/Logo/LN_Logo.svg';
 
 const Header = () => {
   return (
     <div className="w-full h-12 bg-white overflow-hidden fixed top-0 z-10">
       <div className="max-w-[1440px] h-full mx-auto relative flex items-center">
-        <img className="w-9 h-8 absolute left-[86px] top-[10px]" src="https://placehold.co/36x31" alt="Logo" />
+        <img className="w-9 h-8 absolute left-[86px] top-[10px]" src={Logo} alt="Logo" />
         <div className="flex absolute right-[86px]">
           <div className="mr-16 text-black text-xs font-['Lato']">GALLERY</div>
           <div className="mr-16 text-black text-xs font-['Lato']">FEATURED WORKS</div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
+import Logo from '../assets/Logo/Logo_White.png';
 
 const Home = () => {
   return (
-    <div className="w-full relative bg-[--color-gray-200] overflow-hidden">
-      {/* Content Section */}
-      <div className="max-w-[1440px] mx-auto relative pt-12">
-        {/* Text Boxes */}
-        <div className="relative pt-[69px]">
-          <div className="w-20 h-40 absolute left-[89px] bg-[--color-neutral-100]" />
-          <div className="w-36 h-40 absolute left-[134.97px] bg-[--color-neutral-100]" />
-          <div className="absolute left-[135px] top-[161px] text-[--color-neutral-400] text-4xl font-normal font-[--font-family-lato]">Hi, I'm Lynn Xing</div>
-          <div className="absolute left-[137px] top-[213px] text-[--color-neutral-700] text-2xl font-black font-[--font-family-lato]">Visual Storytelling | Spatial Planning | Brand Expression</div>
-        </div>
-        
-        {/* Background Image */}
-        <div className="relative">
-          <img className="w-full h-[810px] mt-[60px]" src="https://placehold.co/1440x810" alt="Background" />
-          
-          {/* Navigation arrows */}
-          <div className="w-9 h-16 absolute right-[55px] bottom-[166px] bg-[--color-white] flex items-center justify-center">
-            <span>→</span>
-          </div>
-          <div className="w-9 h-16 absolute left-[57px] bottom-[100px] bg-[--color-white] flex items-center justify-center">
-            <span>←</span>
-          </div>
-        </div>
+    <div className="w-[1440px] h-[1115px] relative bg-[#F7F7F7] overflow-hidden">
+      {/* Content Section - Positioned absolutely based on the Figma spec */}
+      <div className="w-20 h-40 left-[89px] top-[81px] absolute bg-[#EDEDED]" />
+      <div className="w-36 h-40 left-[134.97px] top-[81px] absolute bg-[#EDEDED]" />
+      {/* Logo on top of the gray area */}
+      <img 
+        className="w-[185px] h-[160px] left-[90px] top-[81px] absolute" 
+        src={Logo} 
+        alt="Lynn Xing Logo" 
+      />
+      
+      <div className="left-[135px] top-[161px] absolute text-[#A5A5A5] text-4xl font-normal font-['Lato']">Hi, I'm Lynn Xing</div>
+      <div className="left-[137px] top-[213px] absolute text-[#424242] text-2xl font-black font-['Lato']">Visual Storytelling | Spatial Planning | Brand Expression</div>
+      
+      {/* Background Image - Positioned absolutely as per Figma spec */}
+      <img className="w-[1440px] h-[810px] left-0 top-[272px] absolute" src="https://placehold.co/1440x810" alt="Background" />
+      
+      {/* Navigation arrows */}
+      <div className="w-9 h-16 left-[1385px] top-[644px] absolute bg-white flex items-center justify-center">
+        <span>→</span>
+      </div>
+      <div className="w-9 h-16 left-[57px] top-[710px] absolute origin-top-left -rotate-180 bg-white flex items-center justify-center">
+        <span>←</span>
       </div>
     </div>
   );
